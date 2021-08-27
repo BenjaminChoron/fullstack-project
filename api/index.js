366479f347a6dd4c8516402195be0a647bdc67c3
BenjaminChoron <benjamin.choron@outlook.com>
2021-08-27 17:08:58 +0200

    add api folder architecture

---
+++ api/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const router = require('./app/router')
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
