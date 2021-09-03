336069e528941ef247cd45e4f21ab5e155d7c779
BenjaminChoron <benjamin.choron@outlook.com>
2021-09-03 14:06:16 +0200

    add packages and config

--- backoffice/src/main.js
+++ backoffice/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './routes'
+import CKEditor from '@ckeditor/ckeditor5-vue2'
+import dayjs from 'dayjs'
+require('dayjs/locale/fr');
+dayjs.locale('fr');
+let advancedFormat = require('dayjs/plugin/advancedFormat');
+dayjs.extend(advancedFormat);
+import VueDayjs from 'vue-dayjs-plugin'
 
 Vue.config.productionTip = false
 
+Vue.use(CKEditor)
+
+Vue.use(VueRouter)
+
+Vue.use(VueDayjs)
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+})
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
