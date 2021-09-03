e3092ed0ea4ea2912aa67fab518194bfc7eb769e
BenjaminChoron <benjamin.choron@outlook.com>
2021-09-03 14:15:44 +0200

    add menu component and style

--- backoffice/src/App.vue
+++ backoffice/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
+    <Menu></Menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Menu
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import './assets/scss/style.scss';
 </style>
---
+++ backoffice/src/components/Menu.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="menu">
+        <h1 class="menu__title">Backoffice</h1>
+        <ul class="menu__nav">
+            <li class="menu__nav__item">
+                <router-link class="menu__nav__item__link" to="/">
+                    <i class="fas fa-home"></i>
+                </router-link>
+            </li>
+            <li class="menu__nav__item">
+                <router-link class="menu__nav__item__link" to="/profile">
+                    <i class="fas fa-user"></i>
+                </router-link>
+            </li>
+            <li class="menu__nav__item">
+                <router-link class="menu__nav__item__link" to="/messages">
+                    <i class="fas fa-envelope"></i>
+                </router-link>
+            </li>
+            <li class="menu__nav__item">
+                <router-link class="menu__nav__item__link" to="/posts">
+                    <i class="fas fa-newspaper"></i>
+                </router-link>
+            </li>
+            <li class="menu__nav__item">
+                <router-link class="menu__nav__item__link" to="/projects">
+                    <i class="fas fa-tools"></i>
+                </router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
